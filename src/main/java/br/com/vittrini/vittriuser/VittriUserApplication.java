aa10b92760a79b0e278500b7525d4bd06335369e
Alex Souza <alexsouza_kb@outlook.com>
2024-05-05 14:29:54 -0300

    :tada: init: Initial commit

---
+++ src/main/java/br/com/vittrini/vittriuser/VittriUserApplication.java
@@ -0,0 +1,13 @@
+package br.com.vittrini.vittriuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VittriUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VittriUserApplication.class, args);
+	}
+
+}
